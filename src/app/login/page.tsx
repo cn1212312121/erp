b5dc1d0bbe810333d25d39360caa1541d8331086
cn <157273371+cn1212312121@users.noreply.github.com>
2024-09-09 14:17:20 +0700

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,28 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h3>Login</h3>
-      ยังไม่ได้เป็นสมาชิก{" "}
-      <Link href={partRoute.REGISTER} className="underline text-blue-500">
-        สมัครสมาชิก
-      </Link>
+      <form action="">
+        <h3 className="mb-3 font-bold text-2xl">Log in</h3>
+
+        <input
+          type="text"
+          placeholder="Username"
+          className="border mx-2 px-2"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="border mx-2 px-2"
+        />
+        <button className="border mx-2 px-2">Log in</button>
+      </form>
+
+      <div className="mt-5">
+        ยังไม่ได้เป็นสมาชิก{" "}
+        <Link href={partRoute.REGISTER} className="underline text-blue-500">
+          สมัครสมาชิก
+        </Link>
+      </div>
     </div>
   );
 };
